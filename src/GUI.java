9464f5fde5c226e6a07c69e58e022b2b5a3110c2
Fyaz <faiyaz1999@utexas.edu>
2018-12-16 18:43:09 -0600

    BOOOORED

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,5 @@
 /* Created by Faiyaz Mostofa.
- * Starter Code used from: https://books.trinket.io/thinkjava/appendix-b.html
+ * Some starter code used from: https://books.trinket.io/thinkjava/appendix-b.html
  * The class that manages the GUI of the main program. 
  * To-do:
  * 	- Create a working chat log
@@ -12,14 +12,26 @@ import javax.swing.JFrame;
 
 public class GUI extends Canvas {
 	
+	private TextLog textlog = new TextLog();
+	
 	/* Default Constructor. 
 	 * Start up the GUI for the program. */
 	public GUI() {
 		JFrame frame = new JFrame("Current: ");
+		textlog.push("Hello World!");
         setSize(400, 400);
+        frame.add(textlog);
         frame.add(this);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+	}
+	
+	/* This refreshes repeatedly. */
+	public void paint(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillOval(getWidth()/4, getHeight()/4, getWidth()/2, getHeight()/2);
+		g.setColor(Color.GREEN);
+		textlog.repaint(g);
 	}
 }
---
+++ src/TextLog.java
@@ -0,0 +1,35 @@
+
+import java.awt.Component;
+import java.awt.Graphics;
+
+public class TextLog extends Component {
+	
+	private String log;
+	
+	public TextLog() {
+		log = "";
+		
+	}
+	
+	public void push(String text) {
+		log.concat(text);
+		if(text.length() > 0 && text.charAt(text.length()-1) != '\n') {
+			log.concat("\n");
+		}
+	}
+	
+	public void pop() {
+		int i = log.length()-2;
+		while(i > -1) {
+			if(log.charAt(i) == '\n') {
+				log = log.substring(0,i);
+				break;
+			}
+		}
+	}
+	
+	public void repaint(Graphics g) {
+		g.drawString(log, 100, 100);
+	}
+	
+}
